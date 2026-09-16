70ffd386041ab64ee2a271705ee4e80d7588ce7b
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-09-16 22:43:34 +0700

    Tugas dan Laprak 1 PBO

--- src/Pekan1/Main.java
+++ src/Pekan1/Main.java
@@ -1,11 +1,13 @@
 package Pekan1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		ArrayList<Rekening> daftarRekening = new ArrayList<>();
 		Rekening akunAktif =  null;
 		boolean isRunning = true;
 		
@@ -17,6 +19,7 @@ public class Main {
 			System.out.println("2. Setor Tunai");
 			System.out.println("3. Tarik Tunai");
 			System.out.println("4. Cek Informasi Rekening");
+			System.out.println("5. Ganti Akun Aktif");
 			System.out.println("0. Keluar");
 			System.out.print("Pilih Menu: ");
 			
@@ -31,10 +34,15 @@ public class Main {
 				String nama = input.nextLine();
 				System.out.print("Masukkan Saldo Awal: ");
 				double saldo = input.nextDouble();
-				
-				//Manggil Constructor
-				akunAktif = new Rekening(no, nama, saldo);
-				break; 
+				if (saldo < 5000) {
+					System.out.println("Saldo Awal Tidak Mencukupi. Masukkan Saldo Minimal Rp50.000,00");
+				} else {
+					//Manggil Constructor
+					akunAktif = new Rekening(no, nama, saldo);
+					daftarRekening.add(akunAktif);
+					break; 					
+				}
+					
 				
 			case 2:
 				if (akunAktif == null) {
@@ -42,28 +50,59 @@ public class Main {
 				} else {
 					System.out.print("Masukkan nominal setor: ");
 					double setor = input.nextDouble();
-					akunAktif.setorTunai(setor); //Behavior/Method
+					if (setor < 10000) {
+						System.out.print("Minimal Setor Rp10.000,00");
+					} else {
+						akunAktif.setorTunai(setor); //Behavior/Method
+					}
 				}
 				break;
+				
 			case 3:
 				if (akunAktif == null) {
 					System.out.println("Error: Mohon maaf, Anda belum memiliki nomor rekening");
 				} else {
 					System.out.print("Masukkan nominal tarik: ");
-					double setor = input.nextDouble();
-					akunAktif.tarikTunai(setor); //Behavior/Method
+					double tarik = input.nextDouble();
+					akunAktif.tarikTunai(tarik); //Behavior/Method
 				}
 				break;
+				
 			case 4:
 				if (akunAktif == null) {
 					System.out.println("Error: Anda belum membuka rekening!");
 				} else {
 					akunAktif.cekInformasi();
 				}
+				
+			case 5:
+				if (daftarRekening.isEmpty()) {
+					System.out.println("Belum Ada Rekening yang Terdaftar!");
+				} else {
+					System.out.println("Daftar Rekening Terdaftar");
+					for (int i = 0; i < daftarRekening.size(); i++) {
+						System.out.println("Pilihan Ke-" + (i + 1) + ":");
+						daftarRekening.get(i).cekInformasi();
+						System.out.println("==================================");
+					}
+					
+					System.out.print("Pilih Nomor Index Rekening yang Ingin Diaktifkan (1 - " + daftarRekening.size() + "): ");
+					int indeksPilihan = input.nextInt();
+					
+					if (indeksPilihan >= 1 && indeksPilihan <= daftarRekening.size()) {
+						akunAktif = daftarRekening.get(indeksPilihan - 1);
+						System.out.println("Berhasil Berganti Akun!");
+					} else {
+						System.out.println("Nomor Pilihan Tidak Valid!");
+					}
+				}
+				break;
+				
 			case 0:
 				isRunning = false;
 				System.out.println("Sistem ditutup. Terima kasih!");
 				break;
+				
 			default:
 				System.out.println("Pilihan tidak valid");
 			}
--- src/Pekan1/Rekening.java
+++ src/Pekan1/Rekening.java
@@ -1,22 +1,25 @@
 package Pekan1;
 
+import java.text.NumberFormat;
+
 public class Rekening {
 
 	String nomorRekening;
 	String namaPemilik;
 	double saldo;
+	NumberFormat nf = NumberFormat.getCurrencyInstance();
 	
 	public Rekening(String nomor, String nama, double saldoAwal) {
 		nomorRekening = nomor;
 		namaPemilik = nama;
 		saldo = saldoAwal;
-		System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat dengan saldo Rp" + saldo);
+		System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat dengan saldo Rp" + nf.format(saldo));
 	}
 	
 	public void setorTunai(double nominal) { // min setor 10000
 		if (nominal >= 10000) {
 			saldo += nominal;
-			System.out.println("Setor tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+			System.out.println("Setor tunai Rp" + nf.format(nominal) + " berhasil. Saldo saat ini: Rp" + nf.format(saldo));
 		} else {
 			System.out.println("Gagal: nominal setor harus lebih dari 10000!");
 		}
@@ -26,17 +29,21 @@ public class Rekening {
 		System.out.println("--- INFO REKENING ---");
 		System.out.println("No. Rekening : " + nomorRekening);
 		System.out.println("Nama Pemilik : " + namaPemilik);
-		System.out.println("Saldo Akhir  : Rp" + saldo);
+		System.out.println("Saldo Akhir  : Rp" + nf.format(saldo));
 		System.out.println("--------------------");
 	}
 	
 	// Implementasi Fitur Tarik Tunai (minimal saldo ditarik: Rp10.000,00)
 	public void tarikTunai(double nominal) {
 		if (nominal >= 10000) {
-			saldo -= nominal;
-			System.out.println("Setor tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+			if (saldo < nominal) {
+				System.out.println("Saldo Anda Tidak Cukup");
+			} else {
+				saldo -= nominal;
+				System.out.println("Tarik Tunai Rp" + nf.format(nominal) + " berhasil. Saldo saat ini: Rp" + nf.format(saldo));
+			}
 		} else {
-			System.out.println("Gagal: nominal setor harus lebih dari 10000!");
+			System.out.println("Gagal: nominal tarik harus lebih dari 10000!");
 		}
 	}
 }
